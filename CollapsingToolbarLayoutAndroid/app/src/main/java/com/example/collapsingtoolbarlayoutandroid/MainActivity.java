88c791d96f4ca11db1cd4d1adaa5204846d6a57d
LQVinh9 <LQVinh9@gmail.com>
2022-01-02 20:10:25 +0700

    [AndroidProject] CollapsingToolbarLayoutAndroid

---
+++ CollapsingToolbarLayoutAndroid/app/src/main/java/com/example/collapsingtoolbarlayoutandroid/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.collapsingtoolbarlayoutandroid;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+    }
+}
